4e23b3f04cfcb3ed9c52b003a2c849260301e7ac
ehmaddd <ehmaddd@gmail.com>
2023-06-07 16:33:52 +0500

    Add Day 3 SQL Commands

--- queries.sql
+++ queries.sql
@@ -30,3 +30,47 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT SUM(escape_attempts), neutered FROM animals GROUP BY neutered;
 SELECT MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY neutered;
 SELECT AVG(escape_attempts) FROM animals WHERE (SELECT EXTRACT ('Year' FROM date_of_birth)) BETWEEN 1990 AND 2000 GROUP BY neutered;
+
+SELECT name
+FROM animals A
+JOIN owners O
+ON A.owner_id=O.id
+WHERE O.full_name='Melody Pond';
+
+SELECT A.name
+FROM animals A
+JOIN species S
+ON A.species_id=S.id
+WHERE S.name='Pokemon';
+
+SELECT full_name AS Owner_name, name as Animal_name
+FROM owners
+LEFT OUTER JOIN animals
+ON owners.id = animals.owner_id;
+
+SELECT COUNT(e.species_id), s.name as specie
+FROM animals e
+JOIN species s
+ON e.species_id = s.id
+GROUP BY s.name;
+
+SELECT A.name
+FROM animals A
+JOIN species S
+ON A.species_id = S.id
+JOIN owners O
+ON A.owner_id = O.id
+WHERE S.name='Digimon' AND O.full_name='Jennifer Orwell';
+
+SELECT name
+FROM animals A
+JOIN owners O
+ON A.owner_id = O.id
+WHERE A.escape_attempts=0 AND O.full_name='Dean Wincheste';
+
+SELECT owners.full_name, top_owner.animals_owned
+FROM (select count(*) as animals_owned, owner_id 
+FROM animals
+GROUP BY owner_id
+ORDER BY animals_owned DESC LIMIT 1) as top_owner
+JOIN owners ON owners.id = top_owner.owner_id;
--- schema.sql
+++ schema.sql
@@ -12,3 +12,29 @@ weight_kg DECIMAL
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR;
+
+CREATE TABLE owners (
+id INTEGER GENERATED ALWAYS AS IDENTITY,
+full_name VARCHAR,
+age INTEGER,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+id INTEGER GENERATED ALWAYS AS IDENTITY,
+name VARCHAR,
+PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+ADD PRIMARY KEY (id);
+
+ALTER TABLE animals DROP species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT
+CONSTRAINT fk_species_id REFERENCES species (id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT
+CONSTRAINT fk_owner_id REFERENCES owners (id);
